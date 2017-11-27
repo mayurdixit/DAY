33e5b4659fe779be35d0367cf39d3881847ec092
204048703 <mayur.dixit@ge.com>
2017-11-27 16:50:02 -0500

    compiled

---
+++ backend/dataAccessLayer/src/main/java/utility/Hello.java
@@ -0,0 +1,25 @@
+/********************************************************************************
+ * Copyright (c) 2015-2016 GE Digital. All rights reserved.                     *
+ *                                                                              *
+ * The copyright to the computer software herein is the property of GE Digital. *
+ * The software may be used and/or copied only with the written permission of   *
+ * GE Digital or in accordance with the terms and conditions stipulated in the  *
+ * agreement/contract under which the software has been supplied.               *
+ ********************************************************************************/
+
+package utility;
+
+/**
+ * Created by 204048703 on 11/27/2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello {
+    public static void main(String[] args) {
+        SpringApplication.run(Hello.class, args);
+    }
+
+}
